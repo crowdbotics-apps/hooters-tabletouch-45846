653fba9704a2f2159fdccf9235443cd8638085ea
Crowdbotics <team@crowdbotics.com>
2024-02-05 03:58:13 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hooters-tabletouch-45846",
+  url: "https://hooters-tabletouch-45846.botics.co",
+  api: "https://hooters-tabletouch-45846.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
